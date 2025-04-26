c96c87a4748455005c168289e52e43ea23cd690c
02Adri <palmabenavidesa650@gmail.com>
2025-04-26 10:21:11 -0600

    añadiendo backend

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-const { json } = require("body-parser");
-
 const form = document.getElementById('contactForm');
     form.addEventListener('submit', async (e) => {
       e.preventDefault();
@@ -9,33 +7,24 @@ const form = document.getElementById('contactForm');
       //const reader = new FileReader();
 
      // reader.onloadend = async () => {
-       
+        const data = {
+          name: formData.get('name'),
+          email: formData.get('email'),
+          subject: formData.get('subject'),
+          message: formData.get('message'),
+          file: file ? await tobBse64(file):null, // Convertir a Base64
+          fileName: file?.name || null,
+          fileType:file?.type || null,
+        };
        //Detectar entorno
        const isNetlify=window.location.hostname.includes('netlify.app')
        const Api_URL=isNetlify?'/.netlify/functions/contact':'http://localhost:3001/api/contact'
         try{
-          if(isNetlify){
-            let response
-            const data = {
-              name: formData.get('name'),
-              email: formData.get('email'),
-              subject: formData.get('subject'),
-              message: formData.get('message'),
-              file: file ? await tobBse64(file):null, // Convertir a Base64
-              fileName: file?.name || null,
-              fileType:file?.type || null,
-            };
-             response= await fetch(Api_URL, {
-              method: 'POST',
-              body:json.stringify(data),
-            });
-          }else{
-            response = await fetch(Api_URL, {
-              method: 'POST',
-              body: formData,
-            });
-          }
-       
+          
+        const response = await fetch(Api_URL, {
+          method: 'POST',
+          body: formData,
+        });
 
         const result = await response.json();
         alert(result.message || result.error);
